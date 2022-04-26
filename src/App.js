fddf4b851066bafdb07021cab4c0758b651f0b54
thiagodasilva <thiagotwoseven@gmail.com>
2022-04-26 16:35:38 -0300

    fix: app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Himpact - o Hábito que impacta!
+          
         </p>
       </header>
     </div>

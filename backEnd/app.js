d1a8b9486520035fdfe83a19a180089257fe9327
Túlio Roberto <96267872+tuliorsc10@users.noreply.github.com>
2022-04-29 21:56:27 -0300

    Liberação dos requests

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,10 +1,16 @@
 const express = require ("express");
+const cors = require("cors")
 const clientes = require("./src/models/Cliente");
 // const cliente_controllers = require("./src/controllers/cliente_controllers")
 
+
+
 const app = express();
 
-app.use(express.json())
+app.use(cors())
+
+app.use(express.json()) 
+
 
 const ClienteController = require('./src/controllers/cliente_controllers')
 // const FornecedorController = require('./src/controllers/fornecedor_controllers')
